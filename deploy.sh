a549e92caa8cfa5eb49c74898373d5b24a27d60c
intrlocutr <45053302+intrlocutr@users.noreply.github.com>
2021-08-27 11:33:55 -0400

    Don't zip unneeded libraries or the huge googleapiclient discovery cache (except for youtube v3)

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-# This script simplifies deploying the script as a Lambda function
+# This script simplifies deploying nightcoreify in AWS Lambda
 
 ###### IMPORTANT ######
-# Create a virtual environment first. When running this script for the first time,
-# uncomment the first 3 lines. Recomment them when running this script again!!
+# Create a virtual environment first -- when running this script for the first time,
+# uncomment the 3 commented lines below. Recomment them when running this script in the future!!
 
-#python -m virtualenv deploy
+#python3 -m virtualenv deploy
 #source deploy/bin/activate
-#python -m pip install -r requirements.txt
+#python3 -m pip install -r requirements.txt
+rm -f lambda.zip
 cd deploy/lib/python3.8/site-packages
-zip -r9 ${OLDPWD}/lambda.zip .
+zip -r9 ${OLDPWD}/lambda.zip . -x pip\* dotenv python_dotenv\* googleapiclient/discovery_cache/documents/\*
+zip ${OLDPWD}/lambda.zip googleapiclient/discovery_cache/documents/youtube.v3.json
 cd $OLDPWD
 zip -g lambda.zip nightcorei.py

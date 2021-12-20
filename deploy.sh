37e93acbc9be876d875e73f13962273f1c441913
intrlocutr <45053302+intrlocutr@users.noreply.github.com>
2021-12-20 13:08:03 -0500

    Install virtualenv in deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,9 @@
 
 ###### IMPORTANT ######
 # Create a virtual environment first -- when running this script for the first time,
-# uncomment the 3 commented lines below. Recomment them when running this script in the future!!
+# uncomment the 4 commented lines below. Recomment them when running this script in the future!!
 
+#python3 -m pip install virtualenv
 #python3 -m virtualenv deploy
 #source deploy/bin/activate
 #python3 -m pip install -r requirements.txt

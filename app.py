7324f1b45c760549594c47f811711e914f0ca5ed
rizMehdi <ccmeall@gmail.com>
2022-12-21 18:37:21 +0000

    remove dlownlad

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ if video:
         video_url = info_dict.get("url", None)
         video_id = info_dict.get("id", None)
         video_title = info_dict.get('title', None)
-        downloadedvideo=ydl.download([video])
+        # downloadedvideo=ydl.download([video])
    
     st.write("You selected: ", video_title)
     # download_song(s_id, YT_URL)
@@ -50,14 +50,14 @@ if video:
         st.video(video) 
 
 
-    # img_url="https://www.publicdomainpictures.net/pictures/300000/velka/abstract-wallpaper-15572324177B2.jpg"
-    # img_dimensions= (1920 ,1285 )
-    # pic_path="img.jpg"
-    # with urllib.request.urlopen(urllib.request.Request(pic_url, headers=REQ_HEADERS)) as res, open(pic_path, 'wb') as file:
-    #     file.write(res.read())
+    img_url="https://www.publicdomainpictures.net/pictures/300000/velka/abstract-wallpaper-15572324177B2.jpg"
+    img_dimensions= (1920 ,1285 )
+    pic_path="img.jpg"
+    with urllib.request.urlopen(urllib.request.Request(pic_url, headers=REQ_HEADERS)) as res, open(pic_path, 'wb') as file:
+        file.write(res.read())
 
-    # if st.button('Nightcorerify it'):
-    #     st.write('Why hello there')
-    #     newvideo = create_video( downloadedvideo, str(pic_path), img_dimensions)
+    if st.button('Nightcorerify it'):
+        st.write('Why hello there')
+        newvideo = create_video( downloadedvideo, str(pic_path), img_dimensions)
 
 

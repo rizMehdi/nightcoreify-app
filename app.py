b737885d11043cb4cdd1496c674b96570ee7e028
rizMehdi <ccmeall@gmail.com>
2022-12-21 19:58:38 +0000

    fix var type

--- app.py
+++ app.py
@@ -63,6 +63,6 @@ if video_url:
 
     if st.button('Nightcorerify it'):
         st.write('Why hello there')
-        newvideo = create_video( video, str(img_path), img_dimensions)
+        newvideo = create_video( [video], str(img_path), img_dimensions)
 
 

bd29d898fdd592e6902b084190e87e579b7263cf
rizMehdi <ccmeall@gmail.com>
2022-12-21 17:36:57 +0000

    add strcutre

--- app.py
+++ app.py
@@ -3,4 +3,10 @@ import streamlit as st
 import streamlit.components.v1 as components
 st.set_page_config(page_title="nightcorerify-app",page_icon=None,layout="wide")
 
-st.write("helloworld")
+st.write("hello world")
+YT_URL = st.text_input('Paste a youtube URL below')
+
+if st.button('Nightcorerify it'):
+    st.write('Why hello there')
+
+

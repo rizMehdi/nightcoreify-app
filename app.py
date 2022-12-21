32dce38f5ed5e5f80e726f4bb3e077dfaababd41
rizMehdi <ccmeall@gmail.com>
2022-12-21 20:11:09 +0000

    fix structure

--- app.py
+++ app.py
@@ -30,40 +30,40 @@ if video_url:
     videocont, pad = st.columns(2)
     with videocont:
         st.video(video_url) 
-    
-    dl_opts = {
-            # ytdl can encode the downloaded audio at a specific sample rate
-            'format': 'bestaudio[asr=%d]' % AUDIO_SAMPLE_RATE,
-            'postprocessors': [{
-                # also, it can create an audio file in whatever format on its own
-                'key': 'FFmpegExtractAudio',
-                'preferredcodec': AUDIO_FILE_FORMAT,
-            }],
-            'audioformat': AUDIO_FILE_FORMAT,
-            'outtmpl': "temp.wav",#file_template,
-            'noplaylist': True,  # sanity
-            'nooverwrites': False,  # sanity
-            'cachedir': False,  # ytdl tries to write to ~ which is read-only in lambda
-        }
-
-    with youtube_dl.YoutubeDL(dl_opts) as ydl:
-        info_dict = ydl.extract_info(video_url, download=False)
-        video_url = info_dict.get("url", None)
-        video_id = info_dict.get("id", None)
-        video_title = info_dict.get('title', None)
-        ydl.download([video_id])
-
-    st.write("You selected: ", video_title)
-
-# (id, title, tags)
-    img_url="https://www.publicdomainpictures.net/pictures/300000/velka/abstract-wallpaper-15572324177B2.jpg"
-    img_dimensions= (1920 ,1285 )
-    img_path="img.jpg"
-    with urllib.request.urlopen(urllib.request.Request(img_url, headers=REQ_HEADERS)) as res, open(img_path, 'wb') as file:
-        file.write(res.read())
-
     if st.button('Nightcorerify it'):
         st.write('Why hello there')
+
+    
+        dl_opts = {
+                # ytdl can encode the downloaded audio at a specific sample rate
+                'format': 'bestaudio[asr=%d]' % AUDIO_SAMPLE_RATE,
+                'postprocessors': [{
+                    # also, it can create an audio file in whatever format on its own
+                    'key': 'FFmpegExtractAudio',
+                    'preferredcodec': AUDIO_FILE_FORMAT,
+                }],
+                'audioformat': AUDIO_FILE_FORMAT,
+                'outtmpl': "temp.wav",#file_template,
+                'noplaylist': True,  # sanity
+                'nooverwrites': False,  # sanity
+                'cachedir': False,  # ytdl tries to write to ~ which is read-only in lambda
+            }
+
+        with youtube_dl.YoutubeDL(dl_opts) as ydl:
+            info_dict = ydl.extract_info(video_url, download=False)
+            video_url = info_dict.get("url", None)
+            video_id = info_dict.get("id", None)
+            video_title = info_dict.get('title', None)
+            ydl.download([video_id])
+
+        st.write("You selected: ", video_title)
+
+    # (id, title, tags)
+        img_url="https://www.publicdomainpictures.net/pictures/300000/velka/abstract-wallpaper-15572324177B2.jpg"
+        img_dimensions= (1920 ,1285 )
+        img_path="img.jpg"
+        with urllib.request.urlopen(urllib.request.Request(img_url, headers=REQ_HEADERS)) as res, open(img_path, 'wb') as file:
+            file.write(res.read())
+
         newvideo = create_video( video_id, str(img_path), img_dimensions)
-
-
+        st.write('ready')
